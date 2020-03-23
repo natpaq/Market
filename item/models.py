043496b26220a8481d6d2e2ca045da84cdc12e6c
natpaq <natmpaquin@videotron.ca>
2020-03-23 19:33:12 -0400

    Adding item to db finally works!

--- item/models.py
+++ item/models.py
@@ -4,9 +4,12 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Item(models.Model):
 	owner = models.ForeignKey(User, on_delete=models.CASCADE)
-	image = models.FileField(upload_to='images/', null=True, verbose_name="")
+	image = models.ImageField(upload_to='images/')
 	itemname = models.CharField(max_length=100)
 	descrip = models.CharField(max_length=1000)
 	price = models.DecimalField(max_digits=10, decimal_places=2)
-	inv_count = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100000000)])
+	inv_count = models.PositiveIntegerField()
+
+def __str__(self):
+        return self.owner + self.image + self.itemname + self.descrip + self.price + self.inv_count
 	
--- item/views.py
+++ item/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from .models import Item
@@ -13,8 +14,9 @@ def add_item(request):
 	if request.method == 'POST':
 	   form = forms.AddItemForm(request.POST, request.FILES)
 	   if form.is_valid():
-              item = form.save(commit=False)
-              item.owner = request.user
-              item.save()
+	      item = form.save(commit=False)
+	      item.owner = request.user
+	      item.save()
+	      return HttpResponseRedirect(reverse('index')) 	
 	   context['form'] = form
 	return render(request, 'list_item.html', context)
