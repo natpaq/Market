ccab45e42b7e043d7adf899dd18ff890da06b666
mchll-ln <michellelin6492@hotmail.com>
2020-03-31 06:28:10 -0400

    UI will be fixed

---
+++ django_websocket/asgi.py
@@ -0,0 +1,18 @@
+"""
+ASGI config for django_websocket project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+import django
+from channels.routing import get_default_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_websocket.settings_prod')
+django.setup()
+application = get_default_application()
+
+
