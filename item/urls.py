3ba744716a709262bc4aabe2161d63306c0abbce
natpaq <natmpaquin@videotron.ca>
2020-03-31 13:41:58 -0400

    Fixed empty cart bug and implemented front end for checkout

--- item/urls.py
+++ item/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('remove_from_cart/<id>/', views.remove_from_cart, name="remove_from_cart"),
     path('my_cart', views.my_cart, name="my_cart"),
     path('delete_from_cart/<id>/', views.delete_from_cart, name="delete_from_cart"),
+    path('checkout', views.checkout, name="checkout"),
 ]
