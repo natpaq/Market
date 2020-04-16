50879f03de71ca4ba56377190247b185f50f44a8
mchll-ln <michellelin6492@hotmail.com>
2020-04-16 10:35:21 -0400

    roomsIn

--- chat/admin.py
+++ chat/admin.py
@@ -1,5 +1,3 @@
 from django.contrib import admin
 
-from .models import Message
-
-admin.site.register(Message)
+# Register your models here.
--- chat/views.py
+++ chat/views.py
@@ -1,14 +1,11 @@
-from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from django.utils.safestring import mark_safe
-import json
+#from .forms import RoomForm
 
 def index(request):
     return render(request, 'chat/index.html', {})
 
-@login_required
-def room(request, room_name):
+def room(request, room_name=None):
     return render(request, 'chat/room.html', {
-        'room_name_json': mark_safe(json.dumps(room_name)),
-        'username': mark_safe(json.dumps(request.user.username)),
+        'room_name': room_name
     })
+    
