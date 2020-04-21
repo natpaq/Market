77c9034b34f26c28c6b373d251a604c08e3393db
mchll-ln <michellelin6492@hotmail.com>
2020-04-21 07:59:27 -0400

    readme slightly updated. Comments fixed

--- chat/tests.py
+++ chat/tests.py
@@ -6,25 +6,25 @@ from django.urls import reverse, resolve
 # Create your tests here.
 
 #Test url routing
-class ChatUrls(SimpleTestCase):
+#class ChatUrls(SimpleTestCase):
 
 ##index
-    def test_main(self):
-        this_url = reverse('main_chat')
-        self.assertEquals(resolve(this_url).func, main_chat)
+#     def test_main(self):
+#         this_url = reverse('main_chat')
+#         self.assertEquals(resolve(this_url).func, main_chat)
 
-class ChatViews(unittest.TestCase):
-##room
-    @classmethod
-    def setUp(self):
-        c = Client()
-        url = reverse('room')
+# class ChatViews(unittest.TestCase):
+# ##room
+#     @classmethod
+#     def setUp(self):
+#         c = Client()
+#         url = reverse('room')
     
-    def get_request(self):
-        c = Client()
-        r = c.get(url, {'room_name': 'test', 'username': 'bob' })
-        self.assertEquals(r.status_code, 200)
-        self.assertTemplateUsed(r, 'room.html')
+#     def get_request(self):
+#         c = Client()
+#         r = c.get(url, {'room_name': 'test', 'username': 'bob' })
+#         self.assertEquals(r.status_code, 200)
+#         self.assertTemplateUsed(r, 'room.html')
 
 
 #test input field 
@@ -34,13 +34,17 @@ class ChatViews(unittest.TestCase):
 ##valid input -> proper room
 
 #Test Websocket 
-# class TestMWS(TestCase):
-
+#REMOVE THIS CLASS bC PYTEST AND IMPORT USAGE  
+# class WSTest(TestCase):
+# @pytest.mark.asyncio
+#     async def ConsConn():
+#         con = WebsocketCommunicator(application, '/ws/chat/this/')
+#         connected, subprotocol = await con.connect()
+#         assert connected
+#         await con.disconnect()
 #Test 
-# class TestMWS(TestCase):
-
 # #Test room messages 
-#class TestMsg(TestCase):
+#class MsgTest(TestCase):
 # ##test messages are viewable by everyone in the same room
 #     def same_room(self):
 
